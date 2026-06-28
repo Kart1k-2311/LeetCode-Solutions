01e204f3fc2298e94a344cc1716982dcbc181381
Kart1k-2311 <kartikx2311@gmail.com>
2026-06-28 12:33:34 +0530

    Time: 10 ms (34.78%), Space: 48.9 MB (7.23%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,15 +4,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             arr[i] = nums[i] * nums[i];
         }
-        for (int i = 0; i < arr.length; i++){
-            for (int j = 1; j < arr.length - i; j++) {
-                if (arr[j] < arr[j - 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(arr);
         return arr;
     }
 }
