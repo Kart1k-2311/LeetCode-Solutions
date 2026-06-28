76a1cfeb5ff10afe9da5bdea807f445c5d973a49
Kart1k-2311 <kartikx2311@gmail.com>
2026-06-28 13:51:27 +0530

    Time: 49 ms (69.33%), Space: 149.2 MB (16.44%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -5,7 +5,7 @@ class Solution {
         int i = n-1;
         long sum = 0;
         while(k>0){
-            if(mul>0){
+            if(mul>0 && i >= 0){
                 sum = sum + (long)nums[i]*mul;
                 k--;
                 i--;
