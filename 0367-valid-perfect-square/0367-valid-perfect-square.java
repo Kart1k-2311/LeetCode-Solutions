a315df91aa098889cb3e0cf0fe64b7c8725becce
Kart1k-2311 <kartikx2311@gmail.com>
2026-07-05 02:01:35 +0530

    Time: 57 ms (6.38%), Space: 41.9 MB (87.39%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,10 +1,28 @@
+// class Solution {
+//     public boolean isPerfectSquare(int num) {
+//         double ans = Math.sqrt(num);
+//         if(ans == (int)ans){
+//             return true;
+//         } else{
+//             return false;
+//         }
+//     }
+// }
 class Solution {
     public boolean isPerfectSquare(int num) {
-        double ans = Math.sqrt(num);
-        if(ans == (int)ans){
-            return true;
-        } else{
-            return false;
+        int start = 1;
+        int end=num;
+        while(start<=end){
+            int mid = start + (end-start) / 2;
+            long ans = (long) mid * mid;
+            if(num == ans){
+                return true;
+            } else if(ans>num){
+                end = mid-1;
+            } else{
+                start = mid +1;
+            }
         }
+        return false;
     }
 }
